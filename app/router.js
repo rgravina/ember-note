d90f949f4364b00581b2949bcccb93b0b1554a25
Robert Gravina <robert.gravina@gmail.com>
2015-07-21 14:01:19 +0900

    List and add notebooks.

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('register');
   this.route('login');
-  this.route('notebooks');
+  this.route('notebooks', {path: "notebooks/:user_id"}, function(){
+    this.route("notes", {path:"notes/:notebook_id"}, function() {});
+  });
 });
 
 export default Router;
